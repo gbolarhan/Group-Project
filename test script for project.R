aebbf7615a84b50aa7d99928278c1ea8d454f9ab
TNIstruct <152735010+TNIstruct@users.noreply.github.com>
2024-05-28 12:48:09 -0400

    Add files via upload

---
+++ test script for project.R
@@ -0,0 +1,29 @@
+library(tidyverse)
+library(ggplot2)
+library(shiny)
+library(dplyr)
+#load the read library for functions
+library(readr)
+library(fBasics)
+library(grid)
+library(gridExtra)
+library(datasets)
+
+#Set my working directory
+path <-setwd("~/HULT/Introduction-R/Project")
+
+#Define CloudWatch Variable to read the CSV File
+CloudWatch <- read_csv("CloudWatch_Traffic_Web_Attack.csv")
+
+#View the CSV File to see the actual data
+data()
+View(CloudWatch)
+glimpse(CloudWatch)
+#Give me the list of all columns/variables/Headers with the datatype classification of the CSV file
+spec(CloudWatch)
+
+#define variable (country = X, count of country codes = y)
+(Country <- CloudWatch$src_ip_country_code)
+
+ggplot(CloudWatch, aes(), )
+
